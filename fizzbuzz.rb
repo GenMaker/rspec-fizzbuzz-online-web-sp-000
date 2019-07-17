e3b0666f8ef467d5cd950a001162b84a0bb5db67
Panela Leung <leungpanela@gmail.com>
2019-07-17 03:21:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 3 == 0
-   print "Fizz"
+   puts "Fizz"
  elsif int % 5 == 0
-    print "Buzz"
+    puts "Buzz"
  elsif int % 3 == 0 && int %5 == 0
-  print "FizzBuzz"
+  puts "FizzBuzz"
   end
 end
